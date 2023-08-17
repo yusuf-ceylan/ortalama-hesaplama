60b27136634d8931dd071c51d77308591d953eee
Yusuf CEYLAN <yusuf.ceylann@outlook.com.tr>
2023-08-17 09:17:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ortalama_hesaplama
+{
+    public class FibonacciCalculator
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n <= 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static double CalculateFibonacciAverage(int depth)
+        {
+            if (depth <= 0)
+                return 0;
+
+            double sum = 0;
+            for (int i = 0; i < depth; i++)
+            {
+                sum += Fibonacci(i);
+            }
+
+            return sum / depth;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fibonacci serisi derinliği: ");
+            int depth = int.Parse(Console.ReadLine());
+
+            double average = FibonacciCalculator.CalculateFibonacciAverage(depth);
+
+            Console.WriteLine($"Fibonacci serisinin ilk {depth} teriminin ortalaması: {average}");
+        }
+    }
+}
